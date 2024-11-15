fde9e9adfb137481fe9b0bb2be98a8bfe74204be
Daniel McCoy <mccoydaniel142@gmail.com>
2024-11-14 20:16:35 -0500

    Created session 4 - Agents, actors, authentication, and permissioned calling.

--- frontend/frontend/MainPage.tsx
+++ frontend/frontend/MainPage.tsx
@@ -1,8 +1,52 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import { HttpAgent, Actor } from "@dfinity/agent";
+import { PlugLogin, Types } from "ic-auth";
+import * as hello from "./interfaces/hello";
 
 export function MainPage() {
 
-  const filler = "This is content!";
+  const whitelist = ["6deh6-waaaa-aaaap-ansea-cai"];
+  const [user, setUser] = useState<Types.UserObject | null>(null);
+
+  // No authentication calling.
+
+  const basicAgent = new HttpAgent({ host: "https://ic0.app" });
+  const basicActor = Actor.createActor(hello.idlFactory, { agent: basicAgent, canisterId: "6deh6-waaaa-aaaap-ansea-cai" });
+
+  const getMessage = async() => {
+    console.log("Calling canister...");
+    const message = await basicActor.getMessage();
+    console.log("My Message: " + message);
+  }
+
+  const setMessage = async() => {
+    console.log("Setting message");
+    const message = document.getElementById("message") as HTMLInputElement;
+    await basicActor.setMessage(message.value);
+    message.value = "";
+    console.log("Message set!");
+  }
+
+  // Plug Wallet and Permissioned Calling
+
+  const plugLogin = async() => {
+    console.log("Logging in...");
+    const userObject = await PlugLogin(whitelist);
+    setUser(userObject);
+    console.log("Logged in!");
+  }
+
+  const getHelloActor = async() => {
+    const actor = Actor.createActor(hello.idlFactory, { agent: user?.agent, canisterId: "6deh6-waaaa-aaaap-ansea-cai" });
+    return actor;
+  }
+
+  const getMesssageWithAuth = async() => {
+    console.log("Calling canister with auth...");
+    const actor = await getHelloActor();
+    const message = await actor.getMessage();
+    console.log("My Message: " + message);
+  }
 
   return (
     <div className="app">
@@ -10,8 +54,16 @@ export function MainPage() {
         <h1>Welcome!</h1>
       </div>
       <div className="content">
-        <p>{filler}</p>
-        <button onClick={() => {alert("Hello World!");}}>Say Hello!</button>
+        <h3>Unauthorized Calling</h3>
+        <button onClick={getMessage}>Get Message</button>
+        <input type="text" id="message" placeholder="Enter message here"></input>
+        <button onClick={setMessage}>Set Message</button>
+      </div>
+      <div className="content">
+        <h3>IC Auth Example</h3>
+        <button onClick={plugLogin}>Plug Login</button>
+        Logged In As: {user ? user.principal : "Not Logged In"}
+        <button onClick={getMesssageWithAuth}>Get Message</button>
       </div>
     </div>
   )
--- hello/src/index.ts
+++ hello/src/index.ts
@@ -1,11 +1,17 @@
 import { IDL, query, update } from 'azle';
+import { ic } from 'azle/experimental';
 
 export default class {
     message: string = 'Hello world!';
+    owner: string = "7zdi6-6h2gk-g4j54-cigti-iiu4u-lj4vy-bewjf-oouoc-dnlck-fyfy5-aae";
 
     @query([], IDL.Text)
     getMessage(): string {
-        return this.message;
+        if (ic.caller().toText() === this.owner) {
+            return this.message;
+        } else {
+            return "You are not the owner!";
+        }
     }
 
     @update([IDL.Text])
